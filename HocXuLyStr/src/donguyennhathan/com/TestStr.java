aa95cbd86245388f3fbd31271cd11ebbe434f1db
J.K <J.K@DESKTOP-C9BEO2P>
2019-12-24 13:08:44 +0700

    Do Nguyen nhat Han 24/12/2019 First commit

---
+++ HocXuLyStr/src/donguyennhathan/com/TestStr.java
@@ -0,0 +1,12 @@
+package donguyennhathan.com;
+
+public class TestStr {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hôm nay học xử lí chuỗi ");
+		System.out.println("Hello mn");
+		System.out.println("kkk");
+	}
+
+}
